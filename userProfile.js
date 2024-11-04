e7791574dadf9bc28322d813f1238a612a59e147
Your Name <you@example.com>
2024-11-04 17:59:35 +0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,8 @@
+let fullName = "Denver G. Valentino";
+console.log(fullName)
+let Age = "20";
+console.log(Age)
+let favoriteNumber = "24";
+console.log(favoriteNumber)
+let favoriteColor = "Black/White";
+console.log(favoriteColor)
